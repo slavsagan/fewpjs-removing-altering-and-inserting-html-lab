fab5fb3977e37daec02bf9493e86d8eec743db09
Slav <itiksolution@gmail.com>
2022-08-14 14:17:11 +1000

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main').remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'Slav is the champion'
